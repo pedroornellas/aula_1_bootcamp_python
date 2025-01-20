7d70acfa3db6817fa2e0be03a932c6e0bd7220f8
Pedro <admpedro@hotmail.com>
2025-01-20 15:25:13 -0300

    adicionando desafio

--- main1.py
+++ main1.py
@@ -1,1 +1,14 @@
-print("pedro")
+
+# print(int(input("Digite valor: ")) + int(input("Digite valor: ")))
+
+constante_bonus = 1000
+nome = input("Digite o nome: ")
+valor_salario_mensal =int(input("Dgite o valor do salario mensal: "))
+valor_percentual_bonus = float(input("Digite o percentual do bônus: "))
+kpi = constante_bonus + valor_salario_mensal*valor_percentual_bonus
+
+print(f"O usuário {nome}, possui o bônus de {kpi}")
+
+
+
+
